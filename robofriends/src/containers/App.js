fac0238019da65e39ec38cc2bda3cbd4074f90d9
Askat Bakyev <bakyev1991@gmail.com>
2019-09-17 00:56:05 -0400

    robofriends by Andrei N

---
+++ robofriends/src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: ""
+    };
+  }
+
+  onSearchChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robots: users });
+      });
+  }
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name
+        .toLocaleLowerCase()
+        .includes(searchField.toLocaleLowerCase());
+    });
+    return !robots.length ? (
+      <h1 className="tc">Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
